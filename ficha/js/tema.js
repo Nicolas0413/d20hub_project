7dc821264bf34cfcbae7e9f6e5852833af9382aa
Gamahen011 <ricaohgp@gmail.com>
2026-05-28 16:24:34 -0300

    Criação da branch master com o projeto

--- ficha/js/tema.js
+++
@@ -1,9 +0,0 @@
-function trocarTema() {
-  const atual = localStorage.getItem("tema") || "style1";
-  const novo = atual === "style1" ? "style2" : "style1";
-
-  localStorage.setItem("tema", novo);
-
-  // recarrega a página já com o tema certo
-  location.reload();
-}
