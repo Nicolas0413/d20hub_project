bc09707383c1c4d2e30e2e56d1063b2589cf041f
Henrique Gama <ricaohgp@gmail.com>
2026-08-17 00:14:50 -0300

    atualização no asgi.py pq render é burro

--- d20hub/asgi.py
+++ d20hub/asgi.py
@@ -3,12 +3,12 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'd20hub.settings')
 
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import sessoes.routing
-
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
